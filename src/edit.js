0f35fe8088967a624f4ecdb76766ced98d145cac
Mohammed Abdul Aziz <cloudaziz@gmail.com>
2026-07-26 16:13:02 +0600

    Added allowedBlocks, defaultBlock and directInsert prop

--- src/edit.js
+++ src/edit.js
@@ -1,11 +1,16 @@
 import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 
 const ALLOWED_BLOCKS = ['core/paragraph', 'core/heading', 'core/image'];
+const DEFAULT_BLOCK = ['core/image'];
 
 export default function Edit() {
   return (
     <div {...useBlockProps()}>
-      <InnerBlocks allowedBlocks={ALLOWED_BLOCKS} />
+      <InnerBlocks
+        allowedBlocks={ALLOWED_BLOCKS}
+        defaultBlock={DEFAULT_BLOCK}
+        directInsert
+      />
     </div>
   );
 }
