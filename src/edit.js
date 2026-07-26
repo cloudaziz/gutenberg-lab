dfad78a2a98b04b04a65e3931a59efee217ef2da
Mohammed Abdul Aziz <cloudaziz@gmail.com>
2026-07-26 15:19:32 +0600

    feat: implement minimal Group Clone block

--- src/edit.js
+++ src/edit.js
@@ -1,63 +1,9 @@
-import {
-	BlockControls,
-	InnerBlocks,
-	LinkControl,
-	useBlockProps,
-} from '@wordpress/block-editor';
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 
-import {
-	Popover,
-	ToolbarButton,
-	ToolbarGroup,
-} from '@wordpress/components';
-
-import { link } from '@wordpress/icons';
-
-import { useState } from '@wordpress/element';
-
-export default function Edit( { attributes, setAttributes } ) {
-	const { url } = attributes;
-
-	const blockProps = useBlockProps();
-
-	const [ isEditingURL, setIsEditingURL ] = useState( false );
-
-	return (
-		<>
-			<BlockControls>
-				<ToolbarGroup>
-					<ToolbarButton
-						icon={ link }
-						label="Link"
-						isPressed={ isEditingURL }
-						onClick={ () => {
-							setIsEditingURL( ! isEditingURL );
-						} }
-					/>
-				</ToolbarGroup>
-			</BlockControls>
-
-			{ isEditingURL && (
-				<Popover
-					position="bottom center"
-					onClose={ () => setIsEditingURL( false ) }
-				>
-					<LinkControl
-						value={ {
-							url,
-						} }
-						onChange={ ( value ) => {
-							setAttributes( {
-								url: value.url,
-							} );
-						} }
-					/>
-				</Popover>
-			) }
-
-			<div { ...blockProps }>
-				<InnerBlocks />
-			</div>
-		</>
-	);
+export default function Edit() {
+  return (
+    <div {...useBlockProps()}>
+      <InnerBlocks />
+    </div>
+  );
 }
